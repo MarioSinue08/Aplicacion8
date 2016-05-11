71952095596c51a4f9bff3d316f30a1dba13b0ce
MarioSinue08 <mario.dsaupoo3e@gmail.com>
2016-05-11 08:45:30 -0500

    3subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,26 +1,15 @@
 // JavaScript Document
 
-$(document).ready(function(e) {
+$(document).ready(function(e){
     document.addEventListener("deviceready",function(){
 		
-		$('#izquierda').on("swipeleft",function(){
-			navigator.notification. alert("Deslizo a la izquierda",function(){"Aplicacion8","Aceptar"})
-});
-
-$('#derecha').on("swiperight",function(){
-	navigator.notification. confirm("¿Que quieres hacer?",function(opt){
-		switch(opt)
-		{
-			case 1:
-			navigator.notification.beep(1);
-			break;
-			
-			case 1:
-			navigator.notification.vibrate(1000);
-			break;
-		}
-	},"Aplicacion8","Beep,Vibrar,Cancelar");
+		$('#beep').tap(function (){
+			navigator.notification.beep (1);
+		})
 		
-});
-},false);
+		$('#vibrar').tap(function (){
+			navigator.notification.vibrate (1000);
+		})
+		
+	},false);
 });
